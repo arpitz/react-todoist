523cce379b3a8e536b28747aaa78f43c4d39115a
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-05-07 09:55:20 +0530

    Added individual project

--- react-todoist/src/components/Projects.js
+++ react-todoist/src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useSelectedProjectValue, useProjectsValue } from '../context';
+import { IndividualProject } from './IndividualProject';
 
 export const Projects = ({ activeValue = null }) => {
 
@@ -24,7 +25,7 @@ return(
         setSelectedProject(project.projectId);
       }}
     >
-        I am a project !!
+        <IndividualProject project={project} />
     </li>
   ))
 )
