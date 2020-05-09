54fbbe5434509ef8d27e2413637fcd334be31fa2
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-05-09 16:28:01 +0530

    Added quick add task functionality in Add task

--- react-todoist/src/components/AddTask.js
+++ react-todoist/src/components/AddTask.js
@@ -56,12 +56,67 @@ export const AddTask = ({ showAddTaskMain = true,
       {
         showAddTaskMain && (
           <div className='add-task__shallow'
-          data-testid='show-main-action'
-          onClick={() => setShowMain(!showMain)}
+            data-testid='show-main-action'
+            onClick={() => setShowMain(!showMain)}
           >
             <span className='add-task__plus'>+</span>
             <span className='add-task__text'>Add Task</span>
 
+          </div>
+        )
+      }
+
+      {
+        (showMain || showQuickAddTask) && (
+          <div className="  add-task__main"
+            data-testid="add-task-main"
+          >
+            {
+              showQuickAddTask && (
+                <>
+                  <div data-testid='quick-add-task'>
+                    <h2 className='header'>Quick Add Task</h2>
+                    <span className="add-task__cancel-x"
+                      data-testid='add-task-quick-cancel'
+                      onClick={() => {
+                        setShowMain(false);
+                        setShowProjectOverlay(false);
+                        setShowQuickAddTask(false);
+                      }}
+                    >X</span>
+                  </div>
+                </>
+              )}
+            <p>Project Overlay here</p>
+            <p>Task date here</p>
+            <input
+              type='text'
+              className="add-task__content"
+              data-testid="add-task-content"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <button
+              type="button"
+              className='add-task__submit'
+              data-testid="add-task"
+              onClick={() => addTask()}
+            >
+              Add Task
+             </button>
+            {
+              !showQuickAddTask && (
+                <span className="add-task__cancel"
+                  data-testid="add-task-main-cancel"
+                  onClick={() => {
+                    setShowMain(false);
+                    setShowProjectOverlay(false);
+                  }}
+                >
+                  Cancel
+                </span>
+              )
+            }
           </div>
         )
       }
